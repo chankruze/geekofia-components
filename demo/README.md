c9f9336c5b585f94b36321977d870756964f41f9
chankruze <chankruze@gmail.com>
2020-12-01 05:18:08 +0530

    Initial Demo Setup
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ demo/README.md
@@ -0,0 +1,109 @@
+<!--
+Author: chankruze (chankruze@geekofia.in)
+Created: Tue Nov 24 2020 10:53:42 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+-->
+
+# Minimal WBR (Webpack Babel React) Setup
+
+This is a very basic setup for a react app. Unlike CRA (create-react-app), this setup gives access to webpack config directly, so we **DON'T** have to fork the `react-scripts` or tweak `create-react-app` webpack config(s) using `eject` and even we don't have to use `react-app-rewired` package.
+
+Default support for:
+
+- `jsx` (primary), `js`
+- `sass` (primary), `scss` (primary), `css`.
+- `module.sass` (primary), `module.scss` (primary), `module.css`.
+
+Project structure:
+
+```
+├── package.json
+├── public
+│   ├── CNAME
+│   ├── _redirects
+│   ├── index.html
+│   ├── manifest.json
+│   └── robots.txt
+├── src
+│   ├── components
+│   │   ├── App.jsx
+│   │   └── App.module.sass
+│   ├── index.jsx
+│   └── index.sass
+├── .babelrc
+├── .env
+├── .eslintrc.js
+├── .gitignore
+├── .babelrc
+├── .env
+├── .eslintrc.js
+├── .gitignore
+├── .prettierignore
+├── .prettierrc
+├── webpack.config.js
+└── yarn.lock
+```
+
+What's preconfigured:
+
+- babel
+- prettier
+- es-lint
+- `_redirects` rule for routing in netlify hosting
+- `CNAME` for custom domain
+- `dotenv` for declaring environmental variables (No need to append `REACT_APP` prefix to your variables)
+
+#### `_redirects` (public dir)
+
+```
+/*    /index.html   200
+```
+
+This redirect rule above will serve the `index.html` instead of giving a 404 no matter what URL the browser requests on a netlify deployment.
+
+#### `.env` (project root dir)
+
+```
+TEST_VALUE=EnvironmentalVariable
+```
+
+this is accessible as `process.env.TEST_VALUE`
+
+#### `CNAME` (public dir)
+
+```
+example.com
+```
+
+## How to Use ?
+
+### `yarn start`
+
+Runs the app in the development mode.
+Open http://localhost:8080 to view it in the browser.
+
+### `yarn lint`
+
+Apply es-lint check to configured files
+
+### `yarn dev`
+
+First lints the project file then runs the app in the development mode.
+Open http://localhost:8080 to view it in the browser.
+
+### `yarn build`
+
+Builds the app for production to the `dist` folder.
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.
+Your app is ready to be deployed!
+
+Well, if you're here, you definitely know what you're going to do!
+
+### Basic Demo
+
+The minimal setup's demo is [here](https://mwbr.netlify.app/)
+
+[![screenshot](https://res.cloudinary.com/chankruze/image/upload/v1606197207/github/Chankruze-s-Minimal-WBR-Setup.png)](https://mwbr.netlify.app/)
---
+++ demo/webpack.config.js
@@ -0,0 +1,132 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Tue Nov 24 2020 07:00:18 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { merge } = require('webpack-merge');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
+	console.log('mode', mode, 'presets', presets);
+	return merge({
+		devServer: {
+			hotOnly: true
+		},
+		module: {
+			rules: [
+				// Files: js/jsx,
+				// Pipes: babel-loader
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader'
+					}
+				},
+				// Files: sass/scss,
+				// Pipes: css-loader
+				{
+					test: /\.module\.s(a|c)ss$/,
+					use: [
+						isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+						{
+							loader: 'css-loader',
+							options: {
+								modules: true,
+								sourceMap: isDevelopment
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: isDevelopment
+							}
+						}
+					]
+				},
+				// Files: .module.sass/scss,
+				// Pipes: sass-loader
+				{
+					test: /\.s(a|c)ss$/,
+					exclude: /\.module.(s(a|c)ss)$/,
+					use: [
+						isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+						'css-loader',
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: isDevelopment
+							}
+						}
+					]
+				},
+				// Files: .module.css,
+				// Pipes: css-loader
+				{
+					test: /\.module\.css$/,
+					use: [
+						isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+						{
+							loader: 'css-loader',
+							options: {
+								modules: true,
+								sourceMap: isDevelopment
+							}
+						}
+					]
+				},
+				// Files: .css,
+				// Pipes: sass-loader
+				{
+					test: /\.css$/,
+					exclude: /\.module.(sa|sc|c)ss$/,
+					use: [
+						isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+						'css-loader'
+					]
+				},
+				// Files: html,
+				// Pipes: html-loader
+				{
+					test: /\.html$/,
+					use: [
+						{
+							loader: 'html-loader'
+						}
+					]
+				}
+			]
+		},
+		plugins: [
+			new HtmlWebPackPlugin({
+				template: './public/index.html',
+				filename: './index.html',
+				hash: true
+			}),
+			new MiniCssExtractPlugin({
+				filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+				chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+			}),
+			new CopyWebpackPlugin({
+				patterns: [
+					{
+						from: 'public'
+					}
+				]
+			}),
+			new webpack.ProgressPlugin(),
+			new Dotenv()
+		],
+		resolve: {
+			extensions: ['.js', '.jsx', '.scss', '.sass', '.css']
+		}
+	});
+};
