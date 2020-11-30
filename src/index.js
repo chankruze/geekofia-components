3f4529521c6897984d6b42bb49f52d75e0156864
chankruze <chankruze@gmail.com>
2020-12-01 05:15:25 +0530

    Adding Fixed Full Navbar
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Sun Nov 29 2020 15:15:21 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+export { default as FixedFullNavbar } from './navbars/fixed/FixedFullNavbar';
+export { default as FloatingNavbar } from './navbars/floating/FloatingNavbar';
---
+++ src/navbars/fixed/FixedFullNavbar.jsx
@@ -0,0 +1,14 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Sun Nov 29 2020 15:26:38 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import React from 'react';
+
+const FixedFullNavbar = () => {
+	return <div></div>;
+};
+
+export default FixedFullNavbar;
